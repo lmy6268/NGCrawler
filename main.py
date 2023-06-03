f57e51120a4989d4ea1088e2a0530cc0c6b8f7f9
kyuyeonlee <lmy6268@gmail.com>
2023-06-03 22:27:13 +0900

    6.3 커밋 - 중복 다운로드를 방지하기 위한 로직을 마련. - 이미지 다운로드 진행 완료 - 필요한 사진이 맞는지는 검증이 필요.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import naver_crawler as crawler
+import os 
 
-#초기화
+"""
+실제로 크롤링이 진행되는 파이썬 파일
+"""
+
 driver = crawler.initDriver()
-keywords = ["제주여행", "여행인물", "부산여행인물"]  # 검색할 키워드 목록을 작성
+keywords = [f"{i} 인생샷" for i in ["해운대", "부산 카페", "광안리", "서울 카페", "제주 카페",
+                                 "남산", "경주", "제주 오름",  "부산 여행", "강릉 여행", "태안 여행", "경주 여행", 
+                                 "제주도 인스타", "부산 인스타", "경주 인스타", "서울 인스타",
+                                 "파리 인스타", "에펠탑", "서울숲", "뉴욕 인스타", "일본 인스타"
+                                 ]]  # 검색할 키워드 목록을 작성
 baseUrl = "https://search.naver.com/search.naver?where=image&sm=tab_jum&query="
-FORDER_PATH = "/Users/kyuyeonlee/Desktop/naverImg"  # 사진을 저장할 경로
+
+FORDER_PATH = "./naverImg"  # 사진을 저장할 경로
+
+#다운받을 폴더 생성
+if not os.path.exists(FORDER_PATH):
+    os.mkdir(FORDER_PATH)
 
 #크롤링 진행
 crawler.doCrawl(driver,keywords,baseUrl,FORDER_PATH)
--- naver_crawler.py
+++ naver_crawler.py
@@ -8,14 +8,14 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import urllib
-from tqdm import tqdm #진행바 표시 
+from tqdm import tqdm  # 진행바 표시
 import os
 import time  # 딜레이를 주기 위해서
 
 # 이미지 비교를 위해 -> imageHash를 고려가능
 
 
-def initDriver(): 
+def initDriver():
     """ 셀레니움 드라이버 초기화 """
     userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"
     options = webdriver.ChromeOptions()
@@ -26,9 +26,10 @@ def initDriver():
     options.add_argument(f"user-agent={userAgent}")
     options.add_argument("disable-gpu")
     driver = webdriver.Chrome(service=ChromeService(
-        ChromeDriverManager().install() #만약 드라이버가 없다면 자동으로 설치해줌
-        ), options=options)
+        ChromeDriverManager().install()  # 만약 드라이버가 없다면 자동으로 설치해줌
+    ), options=options)
     return driver
+
 
 def scrollDown(driver, wait=.5):
     """ 화면 끝까지 스크롤 함 """
@@ -40,19 +41,25 @@ def scrollDown(driver, wait=.5):
         else:
             break
 
+
 def get_files_count(folder_path):
     """ 특정 폴더가 가지고 있는 파일의 갯수"""
     dirListing = os.listdir(folder_path)
     return len(dirListing)
 
 
-def saveImage(imgUrl:str, keyword:str, idx:int,savePath:str):
+def saveImage(imgUrl: str, keyword: str, idx: int, savePath: str):
     """ URL에 있는 사진을 저장함"""
     imgName = f"{keyword}_{idx+1}.jpg"  # 파일명 지정
-    urllib.request.urlretrieve(imgUrl, f"{savePath}/{imgName}")  # 사진을 저장
+    try:
+        urllib.request.urlretrieve(imgUrl, f"{savePath}/{imgName}")  # 사진을 저장
+    except Exception:
+        with open("./log.txt", 'a' if not os.path.exists("./log.txt") else 'w') as f:
+            f.write(imgUrl)
+            f.close()
 
 
-def loadImgList(driver:webdriver, keyword:str, query:str):
+def loadImgList(driver: webdriver, keyword: str, query: str):
     """ 검색결과에서 이미지 목록만 가져옴 """
     driver.get(f"{query}{keyword}")
     time.sleep(1)
@@ -60,16 +67,31 @@ def loadImgList(driver:webdriver, keyword:str, query:str):
     imgs = driver.find_elements(By.CLASS_NAME, "_listImage")  # 이미지가 있는 태그로 이동
     return imgs
 
-def doCrawl(driver:webdriver,keywords:str,baseUrl:str,forderPath:str):
+
+def doCrawl(driver: webdriver, keywords: str, baseUrl: str, forderPath: str):
     """ 실제로 크롤링을 수행하는 부분 \n 
     -> 입력된 폴더경로에 사진들이 저장된다. 
     \t('keyword_순번.jpg' 형식으로 저장됨.)
     """
-    for keyword in tqdm(keywords, desc="네이버 사진 수집 완료", position=0):
-        idx = 0
-        imgs = loadImgList(driver, keyword, query=baseUrl)
-        for img in tqdm(imgs, desc=f"{keyword} 데이터 수집중", position=1):
-            #     # imgUrl = img.get_property('src').replace('&type=a340','') #원본화질로 받기 위함
-            imgUrl = img.get_property('src') #가로길이가 340픽셀 고정으로 저장됨.
-            saveImage(imgUrl, keyword, idx,forderPath)
-            idx += 1
+    #다운받는 이미지 목록을 저장해두고, 중복저장을 막음.
+    try:
+        p = open("./imgUrl.txt", 'r')
+        imgUrlList = [i.rstrip() for i in p.readlines()]
+        p.close()
+    except Exception:
+        imgUrlList = []
+
+    with open("./imgUrl.txt", 'w' if not os.path.exists("./imgUrl.txt") else 'a') as f:
+        for keyword in tqdm(keywords, desc="네이버 사진 수집 완료", position=0):
+            idx = 0
+            imgs = loadImgList(driver, keyword, query=baseUrl)
+            for img in tqdm(imgs, desc=f"{keyword} 데이터 수집중", position=1):
+                #     # imgUrl = img.get_property('src').replace('&type=a340','') #원본화질로 받기 위함
+                imgUrl = img.get_property('src')  # 가로길이가 340픽셀 고정으로 저장됨.
+                if imgUrl not in imgUrlList:  # 사진 경로가 중복이 아닌 경우에만 다운로드를 진행한다.
+                    f.write(imgUrl+"\n")
+                    imgUrlList.append(imgUrl)
+                    saveImage(imgUrl, keyword, idx, forderPath)
+                    idx += 1
+        f.close()
+    print(f"현재까지 {len(imgUrlList)}장 다운로드 됨")
